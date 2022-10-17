bfcf79f4eb10b8455029cbbaa464226dd631cd64
Александров Илья <ybalnagoi@gmail.com>
2022-10-17 21:57:12 +0300

    добавил функцию group

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from ssl import VERIFY_ALLOW_PROXY_CERTS
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('posts.urls')),
     path('admin/', admin.site.urls),
 ]
