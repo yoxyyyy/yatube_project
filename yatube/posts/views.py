5516c68cd72bae079a0766dd13aa641657bb922e
Александров Илья <ybalnagoi@gmail.com>
2022-10-18 21:28:09 +0300

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse #from django.shortcuts import render
 
 
+def index(request):
+    return HttpResponse('Главная страница')
+
+
+def group_post(request, slug):
+    return HttpResponse(f'Группа номер {slug}')
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from ssl import VERIFY_ALLOW_PROXY_CERTS
 from django.contrib import admin
 from django.urls import include, path
 
