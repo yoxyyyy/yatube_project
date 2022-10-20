68c655d7e5464670ed16f591d7ea7293876541fb
Александров Илья <ybalnagoi@gmail.com>
2022-10-20 19:13:21 +0300

    создал папку templates

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,11 @@
-from django.http import HttpResponse #from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    return render(request, template)
 
 
-def group_post(request, slug):
+def group_posts(request, slug):
     return HttpResponse(f'Группа номер {slug}')
