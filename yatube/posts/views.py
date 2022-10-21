4107397342d4fddda672f547145828d0bce476bd
Александров Илья <ybalnagoi@gmail.com>
2022-10-21 21:45:26 +0300

    title plus

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,8 +4,20 @@ from django.shortcuts import render
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = "Это главная страница проекта Yatube"
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Группа номер {slug}')
+    template = 'posts/group_list.html'
+    title = "Здесь будет информация о группах проекта Yatube"
+    context = {
+        'title': title,
+    }
+    return render(request, template, context)
+
+def posts(request):
+    return HttpResponse(200)
